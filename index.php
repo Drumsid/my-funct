6d86b1ad61c277d8281a988a8050dbaabe6907d5
Солодухин Денис Вячеславович <d_solodukhin@AT.local>
2019-09-27 15:43:31 +0300

    add func array filter

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ require_once 'vendor/autoload.php';
 
 use function Funct\Collection\flatten;
 
+// работа функции map
 
-$users = [
+$users = [ //массив для теста
     ['name' => 'Tirion', 'children' => [
         ['name' => 'Mira', 'birdhday' => '1983-03-23']
     ]],
@@ -21,12 +22,6 @@ $users = [
 ];
 
 
-$a = 'HgYgjTIUjgh';
-
-echo "<pre>";
-// print_r(flatten($users, 3));
-echo "</pre>";
-
 function getChildren($arr){
     
   $res = array_map(function($val){
@@ -37,6 +32,64 @@ function getChildren($arr){
 
 $q = getChildren($users);
 
-echo "<pre>";
-print_r(flatten($q));
+// echo "<pre>"; раскоментить посмотреть результат функции getChildren()
+// print_r($q);
+// echo "</pre>";
+
+
+//==================================================================================
+//работа функции filter
+
+$usersGirls = [
+    ['name' => 'Tirion', 'friends' => [
+        ['name' => 'Mira', 'gender' => 'female'],
+        ['name' => 'Ramsey', 'gender' => 'male']
+    ]],
+    ['name' => 'Bronn', 'friends' => []],
+    ['name' => 'Sam', 'friends' => [
+        ['name' => 'Aria', 'gender' => 'female'],
+        ['name' => 'Keit', 'gender' => 'female']
+    ]],
+    ['name' => 'Rob', 'friends' => [
+        ['name' => 'Taywin', 'gender' => 'male']
+    ]],
+];
+
+
+function getFrends($arr){ // дублирую функцию из верехнего блока map но для параметра 'friends'
+    
+  $res = array_map(function($val){
+      return $val['friends'];
+  }, $arr);
+  return flatten($res); // пока не совсем понял как эта flatten()  функция работает
+}
+
+$frends = getFrends($users);
+
+// echo "<pre>";// раскоментить посмотреть результат функции getFrends()
+// print_r($frends);
+// echo "</pre>";
+
+$filteredUsers = array_filter($frends, function ($frend) {
+    return $frend['gender'] == 'female';
+});
+
+// echo "<pre>";// раскоментить посмотреть результат функции array_filter()
+// print_r($filteredUsers);
+// echo "</pre>";
+
+function getGirlFriends($arr){
+
+	$frends = getFrends($arr); // убираем из массива все лишнее кроме 'friends'
+
+	$filteredUsers = array_filter($frends, function ($frend) { //фильтруем $frends по критерию 'female'
+    	return $frend['gender'] == 'female';
+	});
+	return $filteredUsers;
+}
+
+$girlfrends = getGirlFriends($usersGirls);
+
+echo "<pre>";// раскоментить посмотреть результат функции getGirlFriends()
+print_r($girlfrends);
 echo "</pre>";
